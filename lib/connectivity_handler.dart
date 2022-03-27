4cd4fc48e1f24155798cb862c2ef3174f02d5570
Anik Roy <anik.roy@bracits.com>
2022-03-27 13:06:44 +0600

    update version 0.0.3

--- lib/connectivity_handler.dart
+++ lib/connectivity_handler.dart
@@ -1,4 +1,26 @@
 library connectivity_handler;
 
-export 'package:connectivity_handler/connectivity_handler.dart';
+import 'package:connectivity_handler/src/network_manager.dart';
+import 'package:connectivity_plus/connectivity_plus.dart';
+import 'package:flutter/material.dart';
+import 'package:get/get.dart';
+class ConnectivityHandler extends StatelessWidget{
 
+  final Widget _widget;
+
+  ConnectivityHandler(this._widget,{Key? key}) : super(key: key);
+
+  final _networkManager =  Get.put(NetworkManager());
+
+  @override
+  Widget build(BuildContext context) {
+    return  GetBuilder<NetworkManager>(builder: (builder) {
+      if (_networkManager.connectionStatus==ConnectivityResult.none) {
+        return const Center(child: Text("No internet"));
+      } else {
+        return _widget;
+      }
+    });
+  }
+
+}
