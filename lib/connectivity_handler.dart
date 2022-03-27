e8dc18fc2983c5b5de8682b40c224e69b3bd912c
Anik Roy <anik.roy@bracits.com>
2022-03-27 12:08:06 +0600

    init commit

---
+++ lib/connectivity_handler.dart
@@ -0,0 +1,27 @@
+library connectivity_handler;
+
+import 'package:connectivity_handler/src/network_manager.dart';
+import 'package:connectivity_plus/connectivity_plus.dart';
+import 'package:flutter/cupertino.dart';
+import 'package:get/get.dart';
+
+class ConnectivityHandler extends StatelessWidget{
+
+  final Widget _widget;
+
+  ConnectivityHandler(this._widget,{Key? key}) : super(key: key);
+
+  final _networkManager = NetworkManager();
+
+  @override
+  Widget build(BuildContext context) {
+    return  GetBuilder<NetworkManager>(builder: (builder) {
+      if (_networkManager.connectionStatus==ConnectivityResult.none) {
+        return const Center(child: Text("No internet"));
+      } else {
+        return _widget;
+      }
+    });
+  }
+
+}
---
+++ test/connectivity_handler_test.dart
@@ -0,0 +1,9 @@
+import 'package:flutter_test/flutter_test.dart';
+
+import 'package:connectivity_handler/connectivity_handler.dart';
+
+void main() {
+  test('adds one to input values', () {
+
+  });
+}
